3ff3f8ea719c81627286754dd112189e9a01502d
Егор Поняев <champ001@MacBook-Pro-Egor.local>
2024-12-10 00:17:11 +0300

    incorrect results styles when result matrix very big

--- frontend/js/results.js
+++ frontend/js/results.js
@@ -3,12 +3,5 @@ function goToHome() {
     window.location.href = "/";
 }
 
-// Эта функция будет использоваться для загрузки и отображения результатов
-function displayResults(data) {
-    // Здесь можно будет отобразить результаты, полученные с сервера
-    console.log("Результаты:", data);
-}
-
 // Экспортируем функции для использования на странице результатов
 window.goToHome = goToHome;
-window.displayResults = displayResults;
